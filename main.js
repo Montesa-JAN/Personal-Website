df5ff7df19c3bcef65db174df9a2a374a451d38b
Montesa-JAN <105424880+Montesa-JAN@users.noreply.github.com>
2024-04-02 18:14:03 +0800

    Added Home Page and Project Page

--- main.js
+++
@@ -1,6 +0,0 @@
-const card = document.querySelector('.business-card');
-const arrowButton = document.querySelector('.arrow-button');
-
-arrowButton.addEventListener('click', function() {
-    card.classList.toggle('flip')
-});
